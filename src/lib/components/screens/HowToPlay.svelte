422c0481271a67d6364d9aeb4b65b2d85b0fa91a
Eli Nelson <165221493+Papaya-Voldemort@users.noreply.github.com>
2026-05-19 08:51:41 -0600

    Update screen to have back button

--- src/lib/components/screens/HowToPlay.svelte
+++ src/lib/components/screens/HowToPlay.svelte
@@ -1,20 +1,43 @@
 <script>
   import Card from "../core/Card.svelte";
+  import Button from "../core/Button.svelte";
 
+  import { updateScreen } from "../../stores/game";
+  
   const name = "How to Play";
 </script>
 
 <Card>
-  <div>
-    <h1>{name}</h1>
+  <div class="main">
+    <div class="header">
+      <h1>{name}</h1>
+      <div>
+        <Button
+          variant="danger"
+          size="md"
+          style="width: 75px;"
+          onclick={() => {
+            updateScreen("splash");
+          }}>X</Button
+        >
+      </div>
+    </div>
     <p>Coming soon... (with video prob)</p>
   </div>
 </Card>
 
 <style>
-  div {
+  .main {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
   }
 </style>
--- src/lib/components/screens/Info.svelte
+++ src/lib/components/screens/Info.svelte
@@ -1,12 +1,25 @@
 <script>
   import Card from "../core/Card.svelte";
+  import Button from "../core/Button.svelte";
+
+  import { updateScreen } from "../../stores/game";
 
   const name = "Info";
 </script>
 
 <Card>
-  <div>
-    <h1>{name}</h1>
+  <div class="main">
+    <div class="header">
+      <h1>{name}</h1>
+      <div>
+        <Button
+          variant="danger"
+          size="md"
+          style="width: 75px;"
+          onclick={() => {updateScreen("splash")}}>X</Button
+        >
+      </div>
+    </div>
     <p>
       Don't Click That is made for the Congressional App Challenge! I made it
       because I have several family members who have fallen victim to cyber
@@ -17,9 +30,17 @@
 </Card>
 
 <style>
-  div {
+  .main {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
   }
 </style>
